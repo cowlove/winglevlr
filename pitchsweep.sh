b915340fb197dcac64c7eda29fa5ba6df299477b
jim <Jim Evans>
2020-04-26 13:04:56 -0700

    As test flown
    Former-commit-id: acf90d399daf7edb1f2eadaf8b8d59155b4a5db8

--- pitchsweep.sh
+++ pitchsweep.sh
@@ -1,29 +1,26 @@
 #!/bin/bash -x
 
-PN=0
-PU=$(( $PN + 5 ))
-PD=$(( $PN - 5 ))
-SLEEP=10
-for f in {1..3}; do echo dtrk=-1 > /dev/ttyUSB0; done
-for f in {1..3}; do echo roll=0 > /dev/ttyUSB0; done
-for f in {1..3}; do echo maxb=10 > /dev/ttyUSB0; done
+PN=-5
+PU=$(( $PN + 2 ))
+PD=$(( $PN - 2 ))
+SLEEP=30
+for f in {1..3}; do echo dtrk=-1 > /dev/ttyUSB1; done
+for f in {1..3}; do echo roll=0 > /dev/ttyUSB1; done
+for f in {1..3}; do echo maxb=10 > /dev/ttyUSB1; done
 
 for BANK in 0 -10 10; do
-	for f in {1..3}; do echo roll=$BANK > /dev/ttyUSB0; done
-	sleep $SLEEP
-	sleep $SLEEP
-	sleep $SLEEP
-
+	for f in {1..3}; do echo roll=$BANK > /dev/ttyUSB1; done
+	
 	for ITERATION in {1..10}; do
 		echo $ITERATION
-		for f in {1..3}; do echo pitch=$PU > /dev/ttyUSB0; done
+		for f in {1..3}; do echo pitch=$PU > /dev/ttyUSB1; done
 		sleep $SLEEP
-		for f in {1..3}; do echo pitch=$PN > /dev/ttyUSB0; done
+		for f in {1..3}; do echo pitch=$PN > /dev/ttyUSB1; done
 		sleep $SLEEP
-		for f in {1..3}; do echo pitch=$PD > /dev/ttyUSB0; done
+		for f in {1..3}; do echo pitch=$PD > /dev/ttyUSB1; done
 		sleep $SLEEP
-		for f in {1..3}; do echo pitch=$PN > /dev/ttyUSB0; done
+		for f in {1..3}; do echo pitch=$PN > /dev/ttyUSB1; done
 		sleep $SLEEP
 	done
 done
-for f in {1..3}; do echo roll=0 > /dev/ttyUSB0; done
+for f in {1..3}; do echo roll=0 > /dev/ttyUSB1; done
