233a575cc49328168c02a051c594fbcd9f8c936c
jim <Jim Evans>
2020-04-29 14:34:08 -0700

    Add MTIN and PIDL tunable params
    Former-commit-id: 9378f06cd463822ff260d48eb50328e340833ed8
    Former-commit-id: 60583c73a5820686f1c1972f2331b9bb3bbffef6

--- pitchsweep.sh
+++ pitchsweep.sh
@@ -1,26 +1,28 @@
 #!/bin/bash -x
 
-PN=-5
+TTY=/dev/ttyUSB0
+
+PN=0
 PU=$(( $PN + 2 ))
 PD=$(( $PN - 2 ))
 SLEEP=30
-for f in {1..3}; do echo dtrk=-1 > /dev/ttyUSB1; done
-for f in {1..3}; do echo roll=0 > /dev/ttyUSB1; done
-for f in {1..3}; do echo maxb=10 > /dev/ttyUSB1; done
+for f in {1..3}; do echo dtrk=-1 > ${TTY}; done
+for f in {1..3}; do echo roll=0 > ${TTY}; done
+for f in {1..3}; do echo maxb=10 > ${TTY}; done
 
 for BANK in 0 -10 10; do
-	for f in {1..3}; do echo roll=$BANK > /dev/ttyUSB1; done
+	for f in {1..3}; do echo roll=$BANK > ${TTY}; done
 	
 	for ITERATION in {1..10}; do
 		echo $ITERATION
-		for f in {1..3}; do echo pitch=$PU > /dev/ttyUSB1; done
+		for f in {1..3}; do echo pitch=$PU > ${TTY}; done
 		sleep $SLEEP
-		for f in {1..3}; do echo pitch=$PN > /dev/ttyUSB1; done
+		for f in {1..3}; do echo pitch=$PN > ${TTY}; done
 		sleep $SLEEP
-		for f in {1..3}; do echo pitch=$PD > /dev/ttyUSB1; done
+		for f in {1..3}; do echo pitch=$PD > ${TTY}; done
 		sleep $SLEEP
-		for f in {1..3}; do echo pitch=$PN > /dev/ttyUSB1; done
+		for f in {1..3}; do echo pitch=$PN > ${TTY}; done
 		sleep $SLEEP
 	done
 done
-for f in {1..3}; do echo roll=0 > /dev/ttyUSB1; done
+for f in {1..3}; do echo roll=0 > ${TTY}; done
