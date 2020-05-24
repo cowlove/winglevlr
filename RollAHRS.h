4d709ace4ce279f755e147f98e035a6679a3f9a9
jim <Jim Evans>
2020-05-23 21:42:22 -0700

    Add g5 fields to ahrsInfo
    Change data line structure from autotrim.ino
    
    
    Former-commit-id: 13d08efcdeebded10c25fd541d43d592058a14cb

--- RollAHRS.h
+++ RollAHRS.h
@@ -6,20 +6,20 @@ using namespace std;
 
 struct AhrsInput { 
 	float sec, gpsTrack, gpsTrackGDL90, gpsTrackVTG, gpsTrackRMC, alt, p, r, y, ax, ay, az, gx, gy, gz, mx, my, mz, q1, q2, q3, palt, gspeed;
-	float g5Pitch = 0, g5Roll = 0, g5Hdg = 0, g5TimeStamp = 0;
+	float g5Pitch = 0, g5Roll = 0, g5Hdg = 0, g5Ias = 0, g5Tas = 0, g5Palt = 0, g5TimeStamp = 0;
 	String toString() { 
 		static char buf[512];
 		snprintf(buf, sizeof(buf), "%f %.1f %.1f %.1f %.1f %.1f %.3f %.3f %.3f %.3f " /* 1 - 10 */
 			"%.3f %.3f %.3f %.3f %.3f %.3f %.3f %.3f %.3f %.3f " /* 11 - 20 */
-			"%.3f %.3f %.1f %.2f %.2f %.2f %.3f",  /* 21 - 27 */ 
+			"%.3f %.3f %.1f %.2f %.2f %.2f %.2f %.2f %.2f %.3f",  /* 21 - 27 */ 
 		sec, gpsTrack, gpsTrackGDL90, gpsTrackVTG, gpsTrackRMC, alt, p, r, y, ax, ay, az, gx, gy, gz, mx, my, mz, q1, q2, q3, palt, gspeed, 
-		g5Pitch, g5Roll, g5Hdg, g5TimeStamp);
+		g5Pitch, g5Roll, g5Hdg, g5Ias, g5Tas, g5Palt, g5TimeStamp);
 		return String(buf);	
 	 }
 	 AhrsInput fromString(const char *s) { 
-		sscanf(s, "%f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f", 
+		sscanf(s, "%f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f", 
 		&sec, &gpsTrack, &gpsTrackGDL90, &gpsTrackVTG, &gpsTrackRMC, &alt, &p, &r, &y, &ax, &ay, &az, &gx, &gy, 
-		&gz, &mx, &my, &mz, &q1, &q2, &q3, &palt, &gspeed, &g5Pitch, &g5Roll, &g5Hdg, &g5TimeStamp);
+		&gz, &mx, &my, &mz, &q1, &q2, &q3, &palt, &gspeed, &g5Pitch, &g5Roll, &g5Hdg, &g5Ias, &g5Tas, &g5Palt, &g5TimeStamp);
 		return *this;
 	}
 		 
