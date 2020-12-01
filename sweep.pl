233ba0c942266f6cf36a7337347bd8045061e581
jim <Jim Evans>
2020-11-30 19:30:53 -0800

    Added progressive range iterations for the sweep process
    Former-commit-id: 578ff6c38c54cafce50937e75e8c1f97d691eaa8

--- sweep.pl
+++ sweep.pl
@@ -1,54 +1,85 @@
 #!/usr/bin/perl
 
+# time ./sweep.pl NUM=107 sweep.txt 
+
+if ( $ARGV[0] =~ /NUM=(\d+)/i ) {
+	$num = $1;  # Allow NUM= to be overriden on the command line
+	shift;
+}
 
 while(<>) {
 	if (/^\s*[#]/ || /^s*$/) { 
 		#comment  
+    } elsif (/ARGS=(.+)/i) { 
+		$args = $1;
     } elsif (/FIELD=(\d+)/i) { 
 		$field = $1;
 	} elsif (/NUM=(\d+)/i) {
-		$num = $1;
+		if (! defined $num) { 
+			$num = $1;
+		}
 	} else { 
-		($param, $lo, $hi, $inc) = split(' ', $_);
-		$lo{$param} = $lo;
-		$hi{$param} = $hi;
+		($param, $mid, $span, $inc) = split(' ', $_);
+		$span{$param} = $span;
+		$mid{$param} = $mid;
 		$inc{$param} = $inc;
 		push(@params, $param);
 	}
 }
 
-
-foreach $it ( 1..10 ) { 
-	foreach $t ( @params ) {
-		print "Sweep $t:\n";
-		for ($v = $lo{$t}; $v <= $hi{$t}; $v = $v + $inc{$t}) {  
-			$args = ",";
-			foreach $p ( @params ) {
-				if (exists $bestParam{$p} && ($t ne $p)) { 
-					$args = $args . "$p=$bestParam{$p},";
-				} 
-				if ($t eq $p) {
-					$args = $args . "$p=$v,";
-				} 
-			}
-			$cmd = "./winglevlr_ubuntu --replay ./logs/AHRSD$num.DAT --debug \"$args\"";
-			print $cmd . " :\t";
-			if (`$cmd` =~ /#\s([0-9.+-]+)\s([0-9.+-]+)/ ) {
-				if ($field == 1) {
-					$result = $1;
-				} elsif ($field == 2) { 
-					$result = $2;
+$round = 1;	
+while($round < 100) { 
+	$it = 1;
+	while($it < 10) { 
+		foreach $t ( @params ) {
+			delete $best{$t};
+			$span = $span{$t} / $round;
+			$inc = $inc{$t} / $round;
+			$lo = $mid{$t} - $span;
+			$hi = $mid{$t} + $span;
+			print "Sweep '$t' $lo to $hi by $inc, iteration $it:\n";
+			for ($v = $lo; $v <= $hi; $v = $v + $inc) {  
+				$valstr = ",";
+				foreach $p ( @params ) {
+					if (exists $bestV{$p} && ($t ne $p)) { 
+						$valstr = $valstr . "$p=" . sprintf("%.2f", $bestV{$p}) . ",";
+					} 
+					if ($t eq $p) {
+						$valstr = $valstr . "$p=". sprintf("%.2f", $v).",";
+					} 
 				}
+				$cmd = "./winglevlr_ubuntu  $args --replay ./logs/AHRSD$num.DAT --debug \"$valstr\"";
+				print $cmd . " :\t";
+				if (`$cmd` =~ /#\s([0-9.+-]+)\s([0-9.+-]+)/ ) {
+					if ($field == 1) {
+						$result = $1;
+					} elsif ($field == 2) { 
+						$result = $2;
+					}
+				}
+				$totalRuns += 1;
+				print  $result;
+				if (!exists $best{$t} || $result <= $best{$t}) {
+					print " *"; 
+					$best{$t} = $result;
+					$bestV{$t} = $v;
+					$bestCmd = $cmd;
+					$bestScore = $result;
+				}
+				print "\n";
 			}
-			print  $result;
-			if (!exists $best{$t} || $result <= $best{$t}) {
-				print " *"; 
-				$best{$t} = $result;
-				$bestParam{$t} = $v;
-				$bestCmd = $cmd;
-			}
-			print "\n";
 		}
+		if ($bestScore == $lastBest) {
+			print "No improvement, quitting.\n";
+			last;
+		}
+		$lastBest = $bestScore;
+		$it += 1;
+	}
+	print "Result: $bestScore\tIterations: Round: $round\t$it\tTotal simulations: $totalRuns\n$bestCmd\n\n";
+	$round *= 5;
+	foreach $t (keys %bestV) {
+		$mid{$t} = $bestV{$t}
 	}
 }
-print $bestCmd . "\n";
+
