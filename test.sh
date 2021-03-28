67bd6338c1c7eabceb6bb0e896d8644affca369c
Jim Evans <jim@vheavy.com>
2021-03-27 20:40:58 -0700

    working on pitch

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ PROG=./winglevlr_ubuntu
 OUT=testresult.out
 EXPECTED=testexpected.out
 
-rm $PROG
+#rm $PROG
 
 make $PROG && $PROG --replay ./logs/AHRSD006.DAT  | tail -1 > $OUT &&\
 tail -1 $OUT | diff - $EXPECTED
