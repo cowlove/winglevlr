2993517ed04aa7e0ce6a42394381503e98670706
Jim Evans <jim@vheavy.com>
2023-07-09 11:31:46 -0700

    Add quick test_csim script

---
+++ test_csim.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+make winglevlr_ubuntu && ./winglevlr_ubuntu --wind 180@20 --startpos 47.5,-122.45,1000,180,88 --serialInput 1 dtrk=220 --button 180,39,3,0 --log + --seconds 3600 | grep "LOG" > out.plog && ./plot_on_map.py ./out.plog  && ./ll.py ./out.plog map
+
+
